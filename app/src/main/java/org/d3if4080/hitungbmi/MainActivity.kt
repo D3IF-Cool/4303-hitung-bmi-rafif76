4336ce17bc5a79e170b3e26848854528be6cb42d
rafifkhoiron <rafifsicadelpintar@gmail.com>
2021-03-15 21:57:36 +0700

    3.4.Membuat navigasi sederhana

--- app/src/main/java/org/d3if4080/hitungbmi/MainActivity.kt
+++ app/src/main/java/org/d3if4080/hitungbmi/MainActivity.kt
@@ -5,9 +5,21 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
